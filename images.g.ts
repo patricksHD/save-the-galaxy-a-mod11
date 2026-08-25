f73c2d4ce862fedbcb85a8da199934bd1dff5b4f
patricksHD <33277971+patricksHD@users.noreply.github.com>
2026-08-25 16:45:49 +0930

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,48 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "101":return img`
+. 1 . 
+1 . 1 
+1 . 1 
+. 1 . 
+. . . 
+. 1 . 
+. 1 . 
+. 1 . 
+. . . 
+. 1 . 
+1 . 1 
+1 . 1 
+. 1 . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
